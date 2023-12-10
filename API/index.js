42e06e917741016601bef67d9f40a868937be6cf
Mulew <muluken1393@gmail.com>
2023-12-10 12:15:15 +0300

    creating user model

--- API/index.js
+++ API/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 dotenv.config();
 const connectDB = require('./DB/DBConnection');
+//const UserModel = require('./Models/user.model');
 const app = express();
 connectDB();
 
