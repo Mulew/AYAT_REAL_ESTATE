253e5a9af9bc213a4a5f974d7c42f013ef009e00
Mulew <muluken1393@gmail.com>
2023-12-11 07:03:40 +0300

    adding the persistor to store the current user information

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './redux/store.js';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
+import App from './App';
+import { store } from './redux/store';
+import './index.css';
+
+const persistor = persistStore(store);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  <Provider store={store}>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
